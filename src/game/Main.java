735e89eb0e95b50853cf614290f34bd4f47ec051
malinsommar <malinsommar1997@gmail.com>
2020-01-08 12:17:59 +0100

    CastleFight & levelup

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,9 +5,7 @@ import fight.CaveView;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        //MasterModel mm = new MasterModel();
-        //mm.startGame();
-        LevelUpController l = new LevelUpController();
-        l.didPlayerLevelUp();
+        MasterModel mm = new MasterModel();
+        mm.startGame();
     }
 }
