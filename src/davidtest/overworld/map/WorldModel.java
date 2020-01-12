929708b5f6cb08f9f735b25ebb4c61bf2d79d10c
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-01-12 18:24:51 +0100

    to the limit limit

---
+++ src/davidtest/overworld/map/WorldModel.java
@@ -0,0 +1,33 @@
+package davidtest.overworld.map;
+
+import davidtest.overworld.map.WorldController;
+
+    public class WorldModel {
+        private WorldController overWorldController = new WorldController();
+
+        public WorldModel() throws InterruptedException {
+            HandleOverWorld();
+        }
+        public int HandleOverWorld() throws InterruptedException{
+            if (overWorldController.Entrance[0] == 1) {
+                return 1;
+            }
+            if (overWorldController.Entrance[0] == 2) {
+                return 2;
+            }
+            if (overWorldController.Entrance[0] == 3) {
+                return 3;
+            }
+            if (overWorldController.Entrance[0] == 4) {
+                return 4;
+            }
+            if (overWorldController.Entrance[0] == 5) {
+                return 5;
+            }
+            if (overWorldController.Entrance[0] == 6) {
+                return 6;
+            }
+            return 0;
+        }
+
+    }
