b1b26c57f729573d95329d80511697a3aa53fe4f
malinsommar <malinsommar1997@gmail.com>
2020-01-14 21:11:24 +0100

    hry

--- src/fight/ForestFightFrame.java
+++ src/fight/ForestFightFrame.java
@@ -649,14 +649,13 @@ public class ForestFightFrame {
         block.setFont(pixelMplus.deriveFont(30f));
         block.setForeground(Color.black);
         Dimension blockSize = energy.getPreferredSize();
-        block.setBounds(30, 670, blockSize.width, blockSize.height);
+        block.setBounds(30, 670, blockSize.width+50, blockSize.height);
 
         whosTurn = new JLabel("Warrior's turn");
         whosTurn.setFont(pixelMplus.deriveFont(30f));
         whosTurn.setForeground(Color.black);
         whosTurn.setBackground(Color.blue);
         Dimension whoSize = whosTurn.getPreferredSize();
-        //whosTurn.setMaximumSize(new Dimension(whoSize.width+100,whoSize.height+100));
         whosTurn.setBounds(30, 560, whoSize.width, whoSize.height);
     }
 
